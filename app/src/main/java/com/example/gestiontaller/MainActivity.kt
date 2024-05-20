f3095fa1b61b26fcb8bc8462053b24452dd05889
vjp-luismarianoPO <lmpereirao01@informatica.iesvalledeljerteplasencia.es>
2024-05-20 20:18:33 +0200

    Reapply "Update"
    This reverts commit f0fd0e43b6f01f80ba3078dae69426ed6ab2f897.

--- app/src/main/java/com/example/gestiontaller/MainActivity.kt
+++ app/src/main/java/com/example/gestiontaller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,6 +14,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.viewpager2.widget.ViewPager2
 import com.example.gestiontaller.databinding.ActivityMainBinding
 import com.example.gestiontaller.ui.login.LoginActivity
 
