657e6b26ac7a26c2263b570e0e8b007d729ca85d
luismapereira <lmpereirao01@informatica.iesvalledeljerteplasencia.es>
2024-05-23 13:19:03 +0200

    Update

--- app/src/main/java/com/example/gestiontaller/ui/adapter/AccidentAdapter.kt
+++ app/src/main/java/com/example/gestiontaller/ui/adapter/AccidentAdapter.kt
@@ -1,17 +1,25 @@
 package com.example.gestiontaller.ui.adapter
 
+import android.content.Context
 import android.content.DialogInterface.OnClickListener
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gestiontaller.R
 import com.example.gestiontaller.databinding.ItemAccidentBinding
 import com.example.gestiontaller.model.Accident
+import com.example.gestiontaller.services.ApiClient
 import com.example.gestiontaller.ui.viewholder.AccidentViewHolder
 import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Response
 
 class AccidentAdapter(private val accidents: List<Accident>): RecyclerView.Adapter<AccidentViewHolder>() {
 
@@ -26,6 +34,15 @@ class AccidentAdapter(private val accidents: List<Accident>): RecyclerView.Adapt
     override fun onBindViewHolder(holder: AccidentViewHolder, position: Int) {
         val accident = accidents[position]
         holder.render(accident)
+        holder.binding.deleteButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    ApiClient.apiService.deleteAccident(accident.id)
+                } catch (e: Exception) {
+                    //
+                }
+            }
+        }
     }
 
 }
